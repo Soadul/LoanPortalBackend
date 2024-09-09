cfa87c256e476e7001599f548fa089d9b265fb63
Md. Soadul Islam <soadul@ghit.com>
2024-09-09 10:59:59 +0600

    Initial commit

---
+++ loan-application-portal/src/main/java/com/loan/loan_application_portal/LoanApplicationPortalApplication.java
@@ -0,0 +1,13 @@
+package com.loan.loan_application_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApplicationPortalApplication.class, args);
+	}
+
+}
---
+++ loan-application-portal/src/test/java/com/loan/loan_application_portal/LoanApplicationPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan.loan_application_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
